1ffa2b2b7cbd40721eade0872094af9d1a69794b
unknown <resmi32@gmail.com>
2024-03-17 23:49:01 +0530

    nnn

---
+++ index.js
@@ -0,0 +1,19 @@
+//i. import json-server
+const jsonServer=require('json-server')
+//ii. create a server application using a jsonServer
+const restServer=jsonServer.create()
+ //iii. setup path for db.json
+ const router=jsonServer.router('db.json')
+
+ //iv. return middle ware
+ const middleware=jsonServer.defaults()
+
+ //v. setup a port number
+ const port=3001
+
+ //vi.use in restserver 
+restServer.use(middleware)
+restServer.use(router)
+
+ //vii. to run the server
+  restServer.listen(port,()=>{console.log("doctors listing port",+port);})
